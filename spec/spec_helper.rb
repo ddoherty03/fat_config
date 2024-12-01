25e97452026e1246e1e0b3091affda22509a82d1
Daniel E. Doherty <ded@ddoherty.net>
2024-12-01 08:17:35 -0600

    Remove sandbox dir after each rspec example.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -39,6 +39,11 @@ end
 
 RSpec.configure do |config|
   config.include Helper
+
+  config.after do
+    FileUtils.rm_rf(sandbox_dir)
+  end
+
   # Enable flags like --only-failures and --next-failure
   config.example_status_persistence_file_path = ".rspec_status"
 
